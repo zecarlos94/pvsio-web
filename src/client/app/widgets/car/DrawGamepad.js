f07ada02b67c9ad04658aa7ffadcb8f96165a29b
José Carlos Brandão <zecarlos@Joses-MacBook-Pro.local>
2018-03-27 19:40:38 +0100

    finished drawGamepad widget

--- src/client/app/widgets/car/DrawGamepad.js
+++ src/client/app/widgets/car/DrawGamepad.js
@@ -38,7 +38,7 @@ define(function (require, exports, module) {
     // });
 
     let Widget = require("widgets/Widget"),
-        ButtonExternalController = require("widgets/car/ButtonExternalController"),
+        Button = require("widgets/core/ButtonEVO"),
         ButtonActionsQueue = require("widgets/ButtonActionsQueue").getInstance();
 
     /**
@@ -69,6 +69,7 @@ define(function (require, exports, module) {
         this.height = coords.height || 250;
 
         this.parent = (opt.parent) ? ("#" + opt.parent) : "body";
+        this.style = (opt.style) ? (opt.style) : "";
        
         let _this = this;
         let gamepad_file = "text!widgets/car/virtual_controllers/" + opt.style + ".svg";
@@ -84,58 +85,205 @@ define(function (require, exports, module) {
             _this.div.style("transform", "scale(" + ratio + ")");
             _this.gamepad = _this.div.select("svg");
             _this.gamepad.style("transition", "transform 0.3s");
-            if(opt.areas){
-                for(let k=0;k<opt.areas.length;k++){
-                    if(opt.areas[k].type==="circle"){
-                        _this.gamepad.append("circle").attr("id",""+opt.areas[k].id)
-                                                      .attr("cx",""+opt.areas[k].cx)
-                                                      .attr("cy",""+opt.areas[k].cy)
-                                                      .attr("r",""+opt.areas[k].r)
-                                                      .attr("opacity",""+opt.areas[k].opacity)
-                                                      .attr("stroke",""+opt.areas[k].stroke)
-                                                      .attr("stroke-width",""+opt.areas[k].strokeWidth)
-                                                      .attr("href", ""+opt.areas[k].href);
-                    }else if(opt.areas[k].type==="rect"){
-                        _this.gamepad.append("rect").attr("id",""+opt.areas[k].id)
-                                                    .attr("x",""+opt.areas[k].x)
-                                                    .attr("y", ""+opt.areas[k].y)
-                                                    .attr("opacity", ""+opt.areas[k].opacity)
-                                                    .attr("height", ""+opt.areas[k].height)
-                                                    .attr("width", ""+opt.areas[k].width);
-                    }
-                }
-            }
             return _this;
         });
 
         this.div = d3.select(this.parent)
                         .append("div").attr("id", id);
-
-        this.div.append("style").text( " \
-            svg circle { \
-                -webkit-transition: all 2s ease-out; \
-                -moz-transition: all 2s ease-out; \
-                -ms-transition: all 2s ease-out; \
-                -o-transition: all 2s ease-out; \
-                transition: all 2s ease-out; \
-            } \
-            svg circle:active { \
-                opacity: 0.9; \
-            } \
-            svg rect { \
-                -webkit-transition: all 2s ease-out; \
-                -moz-transition: all 2s ease-out; \
-                -ms-transition: all 2s ease-out; \
-                -o-transition: all 2s ease-out; \
-                transition: all 2s ease-out; \
-            } \
-            svg rect:active { \
-                opacity: 0.9; \
-            } \
-            ");
    
         opt.callback = opt.callback || function () {};
         this.callback = opt.callback;
+
+        if(opt.style==="xbox"){
+            this.btn_a= new Button("a", {
+                top: 203, left: 646, width: 32, height: 32
+            }, {
+                keyCode: 49, 
+                callback: opt.callback,
+            });
+
+            this.btn_b= new Button("b", {
+                top: 170, left: 678, width: 32, height: 32
+            }, {
+                keyCode: 50, 
+                callback: opt.callback,
+            });
+
+            this.btn_y= new Button("y", {
+                top: 140, left: 646, width: 32, height: 32
+            }, {
+                keyCode: 51, 
+                callback: opt.callback,
+            });
+
+            this.btn_x= new Button("x", {
+                top: 170, left: 615, width: 32, height: 32
+            }, {
+                keyCode: 52, 
+                callback: opt.callback,
+            });
+
+            this.btn_menu= new Button("menu", {
+                top: 177, left: 568, width: 22, height: 22
+            }, {
+                keyCode: 53, 
+                callback: opt.callback,
+            });
+
+            this.btn_windows= new Button("windows", {
+                top: 177, left: 502, width: 22, height: 22
+            }, {
+                keyCode: 54, 
+                callback: opt.callback,
+            });
+
+            this.btn_xbox= new Button("xbox", {
+                top: 115, left: 525, width: 44, height: 44
+            }, {
+                keyCode: 56, 
+                callback: opt.callback,
+            });
+
+            this.btn_leftArrow= new Button("leftArrow", {
+                top: 248, left: 457, width: 22, height: 22
+            }, {
+                keyCode: 37, 
+                callback: opt.callback,
+            });
+
+            this.btn_upArrow= new Button("upArrow", {
+                top: 228, left: 477, width: 22, height: 22
+            }, {
+                keyCode: 38, 
+                callback: opt.callback,
+            });
+
+            this.btn_rightArrow= new Button("rightArrow", {
+                top: 248, left: 497, width: 22, height: 22
+            }, {
+                keyCode: 39, 
+                callback: opt.callback,
+            });
+
+            this.btn_downArrow= new Button("downArrow", {
+                top: 268, left: 477, width: 22, height: 22
+            }, {
+                keyCode: 40, 
+                callback: opt.callback,
+            });
+
+            this.btn_rightStick= new Button("rightStick", {
+                top: 230, left: 578, width: 52, height: 52
+            }, {
+                keyCode: 57, 
+                callback: opt.callback,
+            });
+
+            this.btn_leftStick= new Button("leftStick", {
+                top: 160, left: 405, width: 52, height: 52
+            }, {
+                keyCode: 48, 
+                callback: opt.callback,
+            });
+        }else if(opt.style==="ps4"){
+            this.btn_cross= new Button("cross", {
+                top: 120, left: 425, width: 32, height: 32
+            }, {
+                keyCode: 49, 
+                callback: opt.callback,
+            });
+
+            this.btn_circle= new Button("circle", {
+                top: 82, left: 463, width: 32, height: 32
+            }, {
+                keyCode: 50, 
+                callback: opt.callback,
+            });
+
+            this.btn_triangle= new Button("triangle", {
+                top: 45, left: 425, width: 32, height: 32
+            }, {
+                keyCode: 51, 
+                callback: opt.callback,
+            });
+
+            this.btn_square= new Button("square", {
+                top: 82, left: 387, width: 32, height: 32
+            }, {
+                keyCode: 52, 
+                callback: opt.callback,
+            });
+
+            this.btn_options= new Button("options", {
+                top: 35, left: 375, width: 20, height: 30
+            }, {
+                keyCode: 53, 
+                callback: opt.callback,
+            });
+
+            this.btn_share= new Button("share", {
+                top: 35, left: 165, width: 20, height: 30
+            }, {
+                keyCode: 54, 
+                callback: opt.callback,
+            });
+
+            this.btn_touchpad= new Button("touchpad", {
+                top: 25, left: 195, width: 170, height: 90
+            }, {
+                keyCode: 55, 
+                callback: opt.callback,
+            });
+
+            this.btn_ps= new Button("ps", {
+                top: 160, left: 268, width: 26, height: 26
+            }, {
+                keyCode: 56, 
+                callback: opt.callback,
+            });
+
+            this.btn_leftArrow= new Button("leftArrow", {
+                top: 87, left: 79, width: 24, height: 24
+            }, {
+                keyCode: 37, 
+                callback: opt.callback,
+            });
+
+            this.btn_upArrow= new Button("upArrow", {
+                top: 60, left: 108, width: 24, height: 24
+            }, {
+                keyCode: 38, 
+                callback: opt.callback,
+            });
+
+            this.btn_rightArrow= new Button("rightArrow", {
+                top: 87, left: 137, width: 24, height: 24
+            }, {
+                keyCode: 39, 
+                callback: opt.callback,
+            });
+
+            this.btn_downArrow= new Button("downArrow", {
+                top: 115, left: 108, width: 24, height: 24
+            }, {
+                keyCode: 40, 
+                callback: opt.callback,
+            });
+
+            this.btn_rightStick= new Button("rightStick", {
+                top: 136, left: 332, width: 62, height: 62
+            }, {
+                keyCode: 57, 
+                callback: opt.callback,
+            });
+
+            this.btn_leftStick= new Button("leftStick", {
+                top: 136, left: 168, width: 62, height: 62
+            }, {
+                keyCode: 48, 
+                callback: opt.callback,
+            });
+        }
 
         Widget.call(this, id, coords, opt);
         return this;
@@ -152,7 +300,7 @@ define(function (require, exports, module) {
      * @instance
      */
     DrawGamepad.prototype.hide = function () {
-        return document.getElementById("drawGamepad").style.display="none";
+        return this.div.style("display","none");
     };
 
     /**
@@ -162,7 +310,7 @@ define(function (require, exports, module) {
      * @instance
      */
     DrawGamepad.prototype.reveal = function () {
-        return document.getElementById("drawGamepad").style.display="block";
+        return this.div.style("display","block");
     };
 
     /**
@@ -171,8 +319,38 @@ define(function (require, exports, module) {
      * @memberof module:DrawGamepad
      * @instance
      */
-    DrawGamepad.prototype.render = function() {
-        return DrawGamepad.prototype.reveal();
+    DrawGamepad.prototype.render = function () {
+        if(this.style==="ps4"){
+            this.btn_cross.render();
+            this.btn_circle.render();
+            this.btn_square.render();
+            this.btn_triangle.render();
+            this.btn_options.render();
+            this.btn_share.render();
+            this.btn_touchpad.render();
+            this.btn_ps.render();
+            this.btn_leftArrow.render();
+            this.btn_upArrow.render();
+            this.btn_rightArrow.render();
+            this.btn_downArrow.render();
+            this.btn_leftStick.render();
+            this.btn_rightStick.render();
+        }else if(this.style==="xbox"){
+            this.btn_a.render();
+            this.btn_b.render();
+            this.btn_x.render();
+            this.btn_y.render();
+            this.btn_menu.render();
+            this.btn_windows.render();
+            this.btn_xbox.render();
+            this.btn_leftArrow.render();
+            this.btn_upArrow.render();
+            this.btn_rightArrow.render();
+            this.btn_downArrow.render();
+            this.btn_leftStick.render();
+            this.btn_rightStick.render();
+        }
+        return this.reveal();
     };
 
     module.exports = DrawGamepad;
